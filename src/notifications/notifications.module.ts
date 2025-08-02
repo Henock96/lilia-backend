5cda9f0a1d85392bc7684c9309860affa4a2d58f
Henock96 <mipokahenok@yahoo.fr>
2025-08-02 03:59:53 +0200

    Update notif2

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule], // Importer le module qui fournit UserService
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   providers: [UserService],
-  controllers: [],
+  controllers: [UsersController],
+  exports: [UserService], // Exporter le service pour que d'autres modules puissent l'injecter
 })
 export class UsersModule {}
