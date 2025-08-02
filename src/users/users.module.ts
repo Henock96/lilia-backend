35bf070709ae69023fb035f43d9f0cfbd3c95af9
Henock96 <mipokahenok@yahoo.fr>
2025-08-02 04:29:53 +0200

    Update notif3

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './users.service';
-import { UsersController } from './users.controller';
+import { AuthController } from './users.controller';
 
 @Module({
   providers: [UserService],
-  controllers: [UsersController],
+  controllers: [AuthController],
   exports: [UserService], // Exporter le service pour que d'autres modules puissent l'injecter
 })
 export class UsersModule {}
