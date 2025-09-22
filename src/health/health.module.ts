facbd7ab4f7d6badb0428ece3fe750f3aa53e3a7
Henock96 <mipokahenok@yahoo.fr>
2025-09-22 18:26:16 +0200

    add firebaseService to health.module

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { FirebaseService } from 'src/firebase/firebase.service';
 
 @Module({
   imports: [],
   controllers: [HealthController],
-  providers: [],
+  providers: [FirebaseService],
   exports: [],
 })
 export class HealthsModule {}
