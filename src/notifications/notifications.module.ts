71d767e5b10632ec5679d2082757014132a7dc8a
Henock96 <mipokahenok@yahoo.fr>
2025-09-22 18:11:15 +0200

    add firebaseService to notification.service

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { FirebaseService } from 'src/firebase/firebase.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, FirebaseService],
   controllers: [NotificationsController],
   providers: [NotificationsService],
   exports: [NotificationsService],
