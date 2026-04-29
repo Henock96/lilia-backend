0ecad6b262b89fef1784ee8028f766c13a0a79d7
Henock96 <mipokahenok@yahoo.fr>
2026-04-29 10:16:09 +0200

    feat: favorites_modules

---
+++ apps/lilia-app/src/modules/favorites/favorites.controller.ts
@@ -0,0 +1,30 @@
+﻿import { Controller, Get, Post, Delete, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { User } from '@prisma/client';
+
+@Controller('favorites')
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  getMyFavorites(@CurrentUser() user: User) {
+    return this.favoritesService.getMyFavorites(user.id);
+  }
+
+  @Post(':restaurantId')
+  addFavorite(@CurrentUser() user: User, @Param('restaurantId') restaurantId: string) {
+    return this.favoritesService.addFavorite(user.id, restaurantId);
+  }
+
+  @Delete(':restaurantId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  removeFavorite(@CurrentUser() user: User, @Param('restaurantId') restaurantId: string) {
+    return this.favoritesService.removeFavorite(user.id, restaurantId);
+  }
+
+  @Get(':restaurantId/check')
+  checkFavorite(@CurrentUser() user: User, @Param('restaurantId') restaurantId: string) {
+    return this.favoritesService.isFavorite(user.id, restaurantId);
+  }
+}
