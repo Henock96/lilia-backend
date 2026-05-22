231fb535c4b60e1a38e865c4861514da0b4b145d
Henok Mipoka <mipokahenok@yahoo.fr>
2026-05-22 01:31:18 +0100

    feat(deliveries): read loyalty earn rate from platform settings

--- apps/lilia-app/src/modules/deliveries/deliveries.module.ts
+++ apps/lilia-app/src/modules/deliveries/deliveries.module.ts
@@ -4,9 +4,10 @@ import { DeliveriesController } from './deliveries.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { OrderStateMachine } from '../orders/order-state.machine';
+import { PlatformSettingsModule } from '../platform-settings/platform-settings.module';
 
 @Module({
-  imports: [PrismaModule, NotificationsModule],
+  imports: [PrismaModule, NotificationsModule, PlatformSettingsModule],
   providers: [DeliveriesService, OrderStateMachine],
   controllers: [DeliveriesController],
   exports: [DeliveriesService],
